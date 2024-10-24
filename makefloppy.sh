f160b1600c276fb3f5b6963af13201128ab8e240
Patrik Sporre <patriksporre@gmail.com>
2024-10-24 20:35:03 +0200

    Created a script to simplify the image creation

---
+++ makefloppy.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# This script creates a 1.44 MB floppy disk image by combining a bootloader and a packed application.
+# 
+# Usage:
+#   ./makefloppy.sh <bootloader> <packed application> <output image>
+#
+# Example:
+#   ./makefloppy.sh boot.bin packed_application.bin floppy.img
+#
+# Note: Make sure the script is executable before running it:
+#   chmod +x makefloppy.sh
+#
+# The script performs the following steps:
+#   1. Creates a blank 1.44 MB floppy disk image (2880 sectors)
+#   2. Writes the bootloader to the first sector (sector 0) of the disk image
+#   3. Writes the packed application to the second sector (sector 2)
+#   4. Outputs a floppy disk image that can be used with QEMU or other emulators
+
+# Check if we have the correct number of arguments
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <bootloader> <packed application> <output image>"
+    exit 1
+fi
+
+# Assign arguments to variables
+BOOTLOADER=$1
+APPLICATION=$2
+OUTPUT=$3
+
+# Create a blank 1.44 MB floppy disk image (2 880 sectors)
+dd if=/dev/zero of="$OUTPUT" bs=512 count=2880
+
+# Write the bootloader to the first sector (sector 0) of the floppy image
+dd if="$BOOTLOADER" of="$OUTPUT" conv=notrunc
+
+# Write the packed application to the second sector (starting at sector 2)
+dd if="$APPLICATION" of="$OUTPUT" bs=512 seek=2 conv=notrunc
+
+# Print a success message
+echo "Floppy disk image created: $OUTPUT"
